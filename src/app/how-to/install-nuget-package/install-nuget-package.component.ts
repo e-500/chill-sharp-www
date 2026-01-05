4f92a4ce2f0dc912d038bf25644431fd488083c7
e-500 <andrea.piovesan@gmail.com>
2026-01-05 13:57:51 +0100

    Updated contents

--- src/app/how-to/install-nuget-package/install-nuget-package.component.ts
+++ src/app/how-to/install-nuget-package/install-nuget-package.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { ContainerComponent } from "../../container/container.component";
 import { TopBarComponent } from "../../top-bar/top-bar.component";
 import { FooterComponent } from "../../footer/footer.component";
-import { HighlightDirective } from '../../highlight.directive';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-install-nuget-package',
-  imports: [ContainerComponent, TopBarComponent, FooterComponent, HighlightDirective, RouterLink],
+  imports: [ContainerComponent, TopBarComponent, FooterComponent, RouterLink],
   templateUrl: './install-nuget-package.component.html',
   styleUrl: './install-nuget-package.component.scss'
 })
