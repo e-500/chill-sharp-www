0cfb5cb08dad49cdf2dc9e92600c2e341fa1e7b3
e-500 <andrea.piovesan@gmail.com>
2026-04-20 21:36:18 +0200

    Shrinked website

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { ContainerComponent } from "../container/container.component";
 import { TopBarComponent } from "../top-bar/top-bar.component";
 import { FooterComponent } from "../footer/footer.component";
-import { RouterLink } from '@angular/router';
-import { HighlightDirective } from '../highlight.directive';
 
 @Component({
   selector: 'app-home',
-  imports: [ContainerComponent, TopBarComponent, FooterComponent, RouterLink, HighlightDirective],
+  imports: [ContainerComponent, TopBarComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
